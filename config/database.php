f3929ce6ef0e4326120e2904e39bc2282d06fb66
Salma <salmaall11233@gmail.com>
2026-05-30 17:18:13 +0300

    fix database connection

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ function getDB() {
     if ($pdo === null) {
         try {
             $pdo = new PDO(
-                'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+                'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4',
                 DB_USER,
                 DB_PASS,
                 [
